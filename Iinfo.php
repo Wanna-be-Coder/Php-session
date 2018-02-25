fed9941f3d4d3045f63e38d347e5f06deafad590
Samin Sharar <36146783+Wanna-be-Coder@users.noreply.github.com>
2018-02-26 05:50:41 +0600

    Add files via upload

--- Iinfo.php
+++ Iinfo.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+if($_SESSION["username"] == null){header("location:index.php");}
+$count =0;
+$index=0;
+$user_Info=array();
+function test_input($data){
+$data=trim($data);
+$data=stripslashes($data);
+$data=htmlspecialchars($data);
+return $data;
+}
+?>
 <!DOCTYPE html>
 <head>
 
@@ -7,6 +20,37 @@
 
 <body>
 	<h1>Iinfo</h1>
+	<?php
+	
+	foreach($_SESSION["login_info"] as $value)
+	{
+		$key = test_input($value);
+		if($_SESSION["username"]==$key)
+		{
+			$index=$count;
+		}
+		$count++;
+	}
+
+
+	$user_Info=array_slice($_SESSION["login_info"],$index,5);
+
+	foreach ($user_Info as $key) {
+		echo "<option>$key</option>" . PHP_EOL ;
+	}
+
+
+	
+
+	?>
+
+	<form action="logout.php" method="GET">
+		<button type="submit" name="logout">Logout</button>
+	</form>
+	<br>
+	<form action="home.php">
+		<button type="submit" name="home">Home</button>
+	</form>
 	
 
 
--- uinfo.php
+++ uinfo.php
@@ -1,13 +1,41 @@
-<!DOCTYPE html>
+<?php
+session_start();
+if($_SESSION["username"] == null){header("location:index.php");}
+
+?>
+<!Doctype html>
+<html>
 <head>
-
-	<title>Welcome to the session Demo</title>
-
+	<title>uinfo</title>
 </head>
 
 <body>
-	<h1>uinfo</h1>
-	
 
+    <h1>User Info<h1>
+	
+	<?php
+			echo $_SESSION["username"];
+
+    ?>
+<br>
+	<?php		
+			
+			echo $_SESSION["password"];
+			
+	?>
+		
+
+
+	</table>
+	
+	<form action="logout.php" method="GET">
+		<button type="submit" name="logout">Logout</button>
+	</form>
+
+	<form action="home.php">
+		<button type="submit" name="home">Home</button>
+	</form>
 
 </body>
+
+</html>
